bde78910c537c66dff80bd1323deb1f4cbac52a6
Neimerc <Neil_Mercury@yahoo.es>
2020-05-10 13:08:29 +0200

    Added MySql scripts and hibernate configuration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HIBERNATE - MAPPING SETS
 This repository holds hibernate code example for mapping sets
+
+See sql-scripts folder. Inside are the SQLs that you need to create in MySql 
---
+++ src/main/java/sql-scripts/hb_student_tracker_student.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE  IF NOT EXISTS `hb_mapping_set`;
+USE `hb_mapping_set`;
+--
+-- Table structure for tables `student` and `image`
+--
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `image`;
+
+CREATE TABLE `image` (
+  `student_id` int(11) NOT NULL,
+  `file_name` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
